1ec2a691e6baaae97382a21c38db8e8b631a07fb
vanza <agustin.vanza@gmail.com>
2022-06-27 19:18:37 -0300

    updated texts

--- src/pages/Info/info.tsx
+++ src/pages/Info/info.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import "./index.css";
 
 const Info = () => {
-  const title = "Informacion util en caso de inundacion";
+  const title = "Información util en caso de inundación";
 
   useEffect(() => {
     const header = document.querySelector("#mobile-header");
@@ -13,12 +13,18 @@ const Info = () => {
     <section id="info">
       <h1 id="info-header">{title}</h1>
       <p>
-        Algunas recomendaciones durante una inundacion
+        Algunas recomendaciones durante una inundación
         <ul>
           <li>Guardar en un buen lugar algo de comida, agua y ropa.</li>
           <li>Mover a un lugar alto los elementos de más valor.</li>
-          <li>Esté preparado para cortar la electricidad cuando haya agua estancada o cables del tendido eléctrico caídos.</li>
-          <li>Colocar documentos importantes en una bolsa de plástico para que no se destruyan con el agua.</li>
+          <li>
+            Esté preparado para cortar la electricidad cuando haya agua
+            estancada o cables del tendido eléctrico caídos.
+          </li>
+          <li>
+            Colocar documentos importantes en una bolsa de plástico para que no
+            se destruyan con el agua.
+          </li>
           <li>Tener a mano una linterna y asegurarse de que tenga pilas.</li>
         </ul>
       </p>
--- src/pages/IssuesInfo/IssuesInfo.tsx
+++ src/pages/IssuesInfo/IssuesInfo.tsx
@@ -14,12 +14,13 @@ const IssuesInfo = () => {
     <section id="issues-info">
       <h1 id="issues-info-header">{title}</h1>
       <p>
-        Mantenganse informado sobre los lugares donde se han hechos denuncias por posibles inundaciones
-        o puede hacer alguna si cree que hay alguna zona de posible inundacion y que otros usuarios puedan informarse.
+        Manténganse informado sobre los lugares donde se han hechos denuncias
+        por posibles inundaciones o puede hacer alguna si cree que hay alguna
+        zona de posible inundación y que otros usuarios puedan informarse.
       </p>
       <p>
-        Las denuncias revisadas y aceptadas, mostraran las coordenas en el mapa de la ciudad de La Plata y un pequeno texto
-        con informacion.
+        Las denuncias revisadas y aceptadas, mostraran las coordenadas en el
+        mapa de la ciudad de La Plata y un pequeño texto con información.
       </p>
       <Button type="button">
         <Link to="/createIssue">Hacer una denuncia</Link>
